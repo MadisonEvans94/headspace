b3802fb928d13c616aa5ecd34750104301cbf400
Madison Evans <m.corbinevans@gmail.com>
2023-12-18 11:49:02 -0700

    initial dummy data model for headspace registered

--- headspace/admin.py
+++ headspace/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
+from .models import HeadSpace, Cluster, Thought
+from django import forms
 
 # Register your models here.
+
+
+class ThoughtAdminForm(forms.ModelForm):
+    class Meta:
+        model = Thought
+        fields = '__all__'
+        widgets = {
+            'cluster': forms.Select(attrs={'required': False}),
+        }
+
+
+class ThoughtAdmin(admin.ModelAdmin):
+    form = ThoughtAdminForm
+
+
+admin.site.register(HeadSpace)
+admin.site.register(Cluster)
+admin.site.register(Thought, ThoughtAdmin)
--- headspace/models.py
+++ headspace/models.py
@@ -1,3 +1,40 @@
 from django.db import models
+from django.core.exceptions import ValidationError
 
-# Create your models here.
+EMBEDDING_VECTOR_LENGTH = 32
+
+# Validators
+
+
+def validate_embedding_vector(value):
+    if len(value) != EMBEDDING_VECTOR_LENGTH:
+        raise ValidationError(
+            f"The list must be exactly {EMBEDDING_VECTOR_LENGTH} elements long.")
+
+    if not all(isinstance(element, float) for element in value):
+        raise ValidationError("All elements of the list must be floats.")
+
+# Models
+
+
+class HeadSpace(models.Model):
+    settings = models.JSONField()
+
+
+class Cluster(models.Model):
+    name = models.TextField(max_length=100)
+    embedding_vector = models.JSONField(
+        validators=[validate_embedding_vector])
+    description = models.TextField(max_length=1000)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True, null=True)
+
+
+class Thought(models.Model):
+    headspace = models.ForeignKey(HeadSpace, on_delete=models.CASCADE)
+    cluster = models.ForeignKey(
+        Cluster, on_delete=models.SET_NULL, null=True)
+    content = models.TextField(max_length=2000)
+    created_at = models.DateTimeField(auto_now_add=True)
+    embedding_vector = models.JSONField(
+        validators=[validate_embedding_vector])
